db8a6c2d5e1f4be08865b51ca8953094c6961d50
GustavoSRost <gustavosrost@outlook.com>
2024-05-16 14:28:37 -0300

    wip

--- index.mjs
+++ index.mjs
@@ -1,15 +1,23 @@
 import { createFile, deleteFile, showFile, updateFile } from './functions.mjs';
 
 async function start() {
-  await createFile(
-    'Conteúdo inicial do arquivo\nCriado com o módulo fs do Node.js'
-  );
-  await showFile();
-  console.log('--------------');
-  await updateFile('Conteúdo modificado...');
-  await showFile();
-  console.log('--------------');
-  await deleteFile();
+  try {
+    await createFile(
+      'Conteúdo inicial do arquivo\nCriado com o módulo fs do Node.js'
+    );
+
+    console.log(await showFile());
+    console.log('--------------');
+
+    await updateFile('Conteúdo modificado...');
+
+    console.log(await showFile());
+    console.log('--------------');
+
+    await deleteFile();
+  } catch (error) {
+    console.error('Ocorreu um erro:', error);
+  }
 }
 
 start();
